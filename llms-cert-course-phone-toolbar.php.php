bf71918c99d7106124da3c2d9d6a94b11d1657c1
Darko Mitrovic <darkomitrovic84@gmail.com>
2021-01-07 20:04:24 +0100

    v1

---
+++ llms-cert-course-phone-toolbar.php.php
@@ -0,0 +1,23 @@
+<?php // Do not copy this line 
+
+// Copy from under this line and paste into your child theme's functions.php
+
+function certificate_buttons_callback( $fields ) {
+    
+	$fields[] = [
+        'name'  => 'phone-number',
+        'icon'  => 'dashicons-phone',
+        'title' => __( 'Phone Number', 'learnpress-certificates' )
+	];
+
+    return $fields;
+}
+add_filter( 'certificates/fields', 'certificate_buttons_callback' );
+
+
+
+class LP_Certificate_Phone_Number_Layer extends LP_Certificate_Layer {
+	public function apply( $data ) {        
+		$this->options['text'] = ! empty( $data['user_id'] ) ? get_user_meta( $data['user_id'], 'lp_info_phone', true ) : $this->options['text'];
+	}
+}
